91fcad5f758966bf06c2ab328823f95b10c92b61
Lukuas31415 <LCBa@web.de>
2025-04-04 17:11:15 +0200

    almost sheet 3

--- learning_lean/Funktionen/Sheet3.lean
+++ learning_lean/Funktionen/Sheet3.lean
@@ -54,25 +54,48 @@ theorem Yb_ne_Yc : Y.b ≠ Y.c := by
 
 -- no cases when they're equal!
 theorem gYb_eq_gYc : g Y.b = g Y.c := by
-  sorry
+  rw[g]
 
 open Function
 
 theorem gf_injective : Injective (g ∘ f) := by
-  sorry
+  intro x y h
+  cases' x <;> cases' y
+  rfl
 
 -- This is a question on the IUM (Imperial introduction to proof course) function problem sheet.
 -- Recall that if you have a hypothesis of the form `h : ∀ A, ...`, then `specialize h X`
 -- will specialize `h` to the specific case `A = X`.
 example : ¬∀ A B C : Type, ∀ (φ : A → B) (ψ : B → C), Injective (ψ ∘ φ) → Injective ψ := by
-  sorry
+  intro h
+  specialize h X Y Z f g
+  --have hg : Injective g := h gf_injective
+  have h1 := gf_injective
+  apply h at h1
+  --have h2 : ∀ x y : Y, g x = g y → x = y := h1
+  change ∀ x y : Y, g x = g y → x = y at h1
+  specialize h1 Y.b Y.c
+  have h3 : g Y.b = g Y.c := by rw[g]
+  apply h1 at h3
+  have h4 : Y.b ≠ Y.c := Yb_ne_Yc
+  apply h4 at h3
+  exact h3
 
 -- Below is another one. Let's make a sublemma first.
 theorem gf_surjective : Surjective (g ∘ f) := by
-  sorry
+  intro x
+  use X.a
 
 -- Another question from IUM
 example : ¬∀ A B C : Type, ∀ (φ : A → B) (ψ : B → C), Surjective (ψ ∘ φ) → Surjective φ := by
-  sorry
+  intro h1
+  specialize h1 X Y Z f g
+  have h2 : Surjective (g∘f) := gf_surjective
+  apply h1 at h2
+  change ∀ y : Y, ∃ x : X, f x = y at h2
+  specialize h2 Y.c
+  --cases h2 with
+  --| X.a =>
+
 
 end Section3sheet1
